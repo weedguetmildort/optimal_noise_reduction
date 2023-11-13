de4a498acac5145157a8b31abd386d0277d0cccc
weedmil <weedmil13@gmail.com>
2023-11-13 10:56:30 -0500

    Update

---
+++ src/main.c
@@ -0,0 +1,5 @@
+//
+// Created by weed1 on 11/13/2023.
+//
+
+#include "main.h"
---
+++ src/main.h
@@ -0,0 +1,8 @@
+//
+// Created by weed1 on 11/13/2023.
+//
+
+#ifndef OPTIMAL_NOISE_REDUCTION_MAIN_H
+#define OPTIMAL_NOISE_REDUCTION_MAIN_H
+
+#endif //OPTIMAL_NOISE_REDUCTION_MAIN_H
